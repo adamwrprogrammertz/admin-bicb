388b57354d17d66c5efdd588760c17aa37cc3c42
Adam Reuben <adamreuben66@gmail.com>
2020-05-16 18:49:43 +0300

    add firebaseconfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyBXfN1wN--7xqy3aKEKtF2RpJxhCXudICc",
+    authDomain: "bicb-50c9c.firebaseapp.com",
+    databaseURL: "https://bicb-50c9c.firebaseio.com",
+    projectId: "bicb-50c9c",
+    storageBucket: "bicb-50c9c.appspot.com",
+    messagingSenderId: "41874637289",
+    appId: "1:41874637289:web:ac22bd2345f22b3fc94b78",
+    measurementId: "G-Z6SSR779CL"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyBXfN1wN--7xqy3aKEKtF2RpJxhCXudICc",
+    authDomain: "bicb-50c9c.firebaseapp.com",
+    databaseURL: "https://bicb-50c9c.firebaseio.com",
+    projectId: "bicb-50c9c",
+    storageBucket: "bicb-50c9c.appspot.com",
+    messagingSenderId: "41874637289",
+    appId: "1:41874637289:web:ac22bd2345f22b3fc94b78",
+    measurementId: "G-Z6SSR779CL"
+  }
 };
 
 /*
