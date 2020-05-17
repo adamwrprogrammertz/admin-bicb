b58a7977c96552155f9716839148165d74a0fced
Innocent Masuki <innocentmasuki99@gmail.com>
2020-05-17 03:56:41 +0300

    changed icons position

--- src/app/side/side.component.ts
+++ src/app/side/side.component.ts
@@ -1,4 +1,32 @@
+import { FlatTreeControl } from '@angular/cdk/tree';
 import { Component, OnInit } from '@angular/core';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
+
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+interface ItemNode {
+  name: string;
+  children?: ItemNode[];
+}
+
+const TREE_DATA: ItemNode[] = [
+  {
+    name: 'Payments',
+    children: [
+      { name: 'Smooth' },
+      { name: 'Disputes' }
+    ]
+  }
+];
+
+/** Flat node with expandable and level information */
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
 
 @Component({
   selector: 'app-side',
@@ -9,13 +37,35 @@ export class SideComponent implements OnInit {
 
   switchLayoutType :"home"|"payment"|"balance"|"customer"|"connected_account"|"reports"|"setting"="home";
 
-  constructor() { }
+  changeLayoutDesign(status) {
+    this.switchLayoutType = status;
+  }
 
+
+  private _transformer = (node: ItemNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  }
+
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level, node => node.expandable);
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer, node => node.level, node => node.expandable, node => node.children);
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
   ngOnInit(): void {
   }
 
-  changeLayoutDesign(status){
-    this.switchLayoutType = status;
-  }
+  
 
 }
