e3bdc9d753826c815bbbd5d055f54d33b977f555
Adam Reuben <adamreuben66@gmail.com>
2020-05-17 03:08:28 +0300

    added design polish for payment all tab

--- src/app/payment/all-payment/all-payment.component.ts
+++ src/app/payment/all-payment/all-payment.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AllFireService } from 'src/app/all-fire.service';
+import { StatusCheckService } from '../../status-check.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-all-payment',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllPaymentComponent implements OnInit {
   panelOpenState = false;
-  constructor() { }
+  customerObjectFromFirebase: any;
+  
+  constructor(public serviceFb:AllFireService,
+    private statuService:StatusCheckService,
+    private afAuth:AngularFireAuth,
+    private snackBar:MatSnackBar,) { }
 
   ngOnInit(): void {
+    this.statuService.progressBarStatus = true;
+    this.serviceFb.getAllCustomerDetail().subscribe(datas=>{
+      if(datas != null){
+        this.customerObjectFromFirebase = datas;
+        this.statuService.progressBarStatus = false;
+      }else{
+        //No data
+      }
+    });
   }
 
 }
