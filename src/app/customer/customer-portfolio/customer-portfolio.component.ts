0858edd92264486c4720f5fafe4c71784d1db31a
Innocent Masuki <innocentmasuki99@gmail.com>
2020-05-17 15:20:27 +0300

    customer portifolio

--- src/app/customer/customer-portfolio/customer-portfolio.component.ts
+++ src/app/customer/customer-portfolio/customer-portfolio.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-export interface Section {
-  name: string;
-  updated: Date;
-}
+
 import { ServiceModelService } from '../../all-data-service/service-model.service';
 
 @Component({
@@ -11,31 +8,7 @@ import { ServiceModelService } from '../../all-data-service/service-model.servic
   styleUrls: ['./customer-portfolio.component.css']
 })
 export class CustomerPortfolioComponent implements OnInit {
-  folders: Section[] = [
-    {
-      name: 'Photos',
-      updated: new Date('1/1/16'),
-    },
-    {
-      name: 'Recipes',
-      updated: new Date('1/17/16'),
-    },
-    {
-      name: 'Work',
-      updated: new Date('1/28/16'),
-    }
-  ];
-  notes: Section[] = [
-    {
-      name: 'Vacation Itinerary',
-      updated: new Date('2/20/16'),
-    },
-    {
-      name: 'Kitchen Remodel',
-      updated: new Date('1/18/16'),
-    }
-  ];
-
+  
   constructor(public allDataService:ServiceModelService) { }
 
   ngOnInit(): void {
